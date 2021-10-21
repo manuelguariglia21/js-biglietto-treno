ce5040c277c5ddb33d0cc05fbb28ddb1c5d3cf41
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-21 14:44:52 +0200

    inizializzazione variabili

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//chiedere all’utente il numero di chilometri che vuole percorrere
+const kilometres = prompt("Quanti KM devi percorrere?");
+console.log('Km:', kilometres);
+//chiedere all’utente l’età del passeggero
+const years = prompt("Quanti anni hai?");
+console.log('Years:', years);
