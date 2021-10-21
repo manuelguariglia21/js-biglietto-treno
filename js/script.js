cb3e6aa0d76248c6dd2fa012e207cee3433cabbb
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-21 14:52:27 +0200

    fix sales

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ console.log('Km:', kilometres);
 //chiedere all’utente l’età del passeggero
 const years = prompt("Quanti anni hai?");
 console.log('Years:', years);
+//prezzo del biglietto è definito in base ai km
+const fullPrice = kilometres*0.21;
+//sconto del 20% per i minorenni
+const salePriceJunior = fullPrice-(fullPrice*0.20);
+//sconto del 40% per gli over 65
+const salePriceSenior = fullPrice-(fullPrice*0.40);
+
+
+
+
+
+
+
