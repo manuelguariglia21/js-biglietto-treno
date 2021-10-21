73f8b6673b799337ba2028287c68d4dc8646fefe
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-21 16:06:19 +0200

    FIX COUPON AND ALERT

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,14 +1,33 @@
 //chiedere all’utente il numero di chilometri che vuole percorrere
 const kilometres = prompt("Quanti KM devi percorrere?");
 console.log('Km:', kilometres);
+
 //chiedere all’utente l’età del passeggero
 const years = prompt("Quanti anni hai?");
 console.log('Years:', years);
+
+//codice sconto
+let coupon = prompt("Inserisci Coupon");
+console.log('Coupon:', coupon);
+
+if(coupon == "SCONTO20" && years <= 20){
+  coupon = true;
+}
+else if(coupon != "SCONTO20" && years <= 20){
+  coupon = false;
+  alert("spiacenti, il codice sconto inserito non è valido.");
+
+}
+else if(coupon == "SCONTO20" && years > 20){
+  coupon = false;
+  alert("spiacenti, il codice sconto inserito non è valido per la sua fascia d'età.");
+
+}
+
 //prezzo del biglietto è definito in base ai km
 let fullPrice = kilometres*0.21;
 
 //booking-area
-
 if(years > 65){
   fullPrice = fullPrice-(fullPrice*0.40);
   document.getElementById('booking').innerHTML = 
@@ -21,16 +40,43 @@ if(years > 65){
   `;
 }
 
-else if(years < 18){
-  fullPrice = fullPrice-(fullPrice*0.20);
+else if(years <= 20){
+
+  if(years < 18 && coupon){
+  fullPrice = fullPrice-(fullPrice*(0.20 + 0.20));
   document.getElementById('booking').innerHTML = 
   `
 
-  <h1>Prezzo Scontato per Under 18!</h1>
+  <h1>Prezzo Scontato per Under 18 e coupon SCONTO20!</h1>
 
   <h2>Il prezzo finale del biglietto è: ${fullPrice.toFixed(2)} &euro;</h2>
 
   `;
+  }
+
+  else if(years < 18){
+    fullPrice = fullPrice-(fullPrice*0.20);
+    document.getElementById('booking').innerHTML = 
+    `
+  
+    <h1>Prezzo Scontato per Under 18!</h1>
+  
+    <h2>Il prezzo finale del biglietto è: ${fullPrice.toFixed(2)} &euro;</h2>
+  
+    `;
+    }
+
+  else if(years >= 18 && coupon){
+  fullPrice = fullPrice-(fullPrice*(0.20));
+  document.getElementById('booking').innerHTML = 
+  `
+
+  <h1>Prezzo Scontato coupon SCONTO20!</h1>
+
+  <h2>Il prezzo finale del biglietto è: ${fullPrice.toFixed(2)} &euro;</h2>
+
+  `;
+  }
 }
 
 else{
